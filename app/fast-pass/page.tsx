744b76f4df99831fc66a6a9a6fe66ea27c6b6304
Rhun Dev <dev@rhun.io>
2025-01-25 22:27:04 -0800

    beta v1

--- app/fast-pass/page.tsx
+++ app/fast-pass/page.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import FastPassComponent from '@/components/fast-pass-component';
 import { CrossmintProvider, CrossmintCheckoutProvider } from '@crossmint/client-sdk-react-ui';
+export const dynamic = 'force-dynamic'
 
 const FastPassPage: React.FC = () => {
     const clientApiKey = process.env.NEXT_PUBLIC_CLIENT_API_KEY as string;
