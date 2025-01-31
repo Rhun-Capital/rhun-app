5c762d7c50e1f875fe04ab239b169904576fde2e
Rhun Dev <dev@rhun.io>
2025-01-30 19:58:10 -0800

    bug fixes

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -85,6 +85,7 @@ export default function HomePage() {
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {features.map((feature) => (
           <div 
+          key={feature.title}
           className="group"
           onClick={(e) => handleFeatureClick(e, feature)}
           >
