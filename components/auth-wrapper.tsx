473135c10b2b08436c6e9801b29dafb14359ee20
Alex <alexdaro34@gmail.com>
2025-02-24 13:11:29 -0800

    remove loading image

--- components/auth-wrapper.tsx
+++ components/auth-wrapper.tsx
@@ -3,7 +3,6 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import { usePrivy } from '@privy-io/react-auth'
 import { usePathname } from 'next/navigation'
-import Image from 'next/image'
 
 interface AuthWrapperProps {
   children: React.ReactNode;
@@ -25,7 +24,6 @@ export function AuthWrapper({ children }: AuthWrapperProps) {
       <div className="min-h-screen flex items-center justify-center">
         {/* <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-indigo-500" /> */}
         <div className="animate-pulse flex flex-col items-center gap-1 justify-center">
-          <div><Image src="/images/profile.png" height={55} width={55} alt="" className="rounded-full mb-1"/></div>
           <div className="text-sm text-zinc-500">Loading</div>
         </div>
       </div>
