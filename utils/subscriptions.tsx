a3958cee0af11710089cbcd03fa11ffbc0edbf67
Rhun Dev <dev@rhun.io>
2026-02-06 10:59:17 -0800

    fix: Update Stripe API version to 2025-02-24.acacia
    The Stripe package was updated as part of the security fix and requires
    the newer API version.

--- utils/subscriptions.tsx
+++ utils/subscriptions.tsx
@@ -5,7 +5,7 @@ import { DynamoDBDocumentClient, GetCommand, PutCommand, UpdateCommand, QueryCom
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-01-27.acacia'
+  apiVersion: '2025-02-24.acacia'
 });
 
 const client = new DynamoDBClient({
